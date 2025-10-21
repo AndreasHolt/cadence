efb9cb5f4a8412a637c090732c094a858a09b3b1
Theis Randeris Mathiassen <theis@tmathiassen.dk>
2025-10-22 00:00:53 +0200

    Added functions to update and get shard metrics

--- service/sharddistributor/store/wrappers/metered/store_generated.go
+++ service/sharddistributor/store/wrappers/metered/store_generated.go
@@ -79,6 +79,16 @@ func (c *meteredStore) GetHeartbeat(ctx context.Context, namespace string, execu
 	return
 }
 
+func (c *meteredStore) GetShardMetrics(ctx context.Context, namespace string, shardIDs []string) (m1 map[string]store.ShardMetrics, err error) {
+	op := func() error {
+		m1, err = c.wrapped.GetShardMetrics(ctx, namespace, shardIDs)
+		return err
+	}
+
+	err = c.call(metrics.ShardDistributorStoreGetShardMetricsScope, op, metrics.NamespaceTag(namespace))
+	return
+}
+
 func (c *meteredStore) GetShardOwner(ctx context.Context, namespace string, shardID string) (s1 string, err error) {
 	op := func() error {
 		s1, err = c.wrapped.GetShardOwner(ctx, namespace, shardID)
@@ -116,5 +126,15 @@ func (c *meteredStore) Subscribe(ctx context.Context, namespace string) (ch1 <-c
 	}
 
 	err = c.call(metrics.ShardDistributorStoreSubscribeScope, op, metrics.NamespaceTag(namespace))
+	return
+}
+
+func (c *meteredStore) UpdateShardMetrics(ctx context.Context, namespace string, executorID string, shardMetrics map[string]store.ShardMetrics) (err error) {
+	op := func() error {
+		err = c.wrapped.UpdateShardMetrics(ctx, namespace, executorID, shardMetrics)
+		return err
+	}
+
+	err = c.call(metrics.ShardDistributorStoreUpdateShardMetricsScope, op, metrics.NamespaceTag(namespace))
 	return
 }
