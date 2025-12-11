4fea4e25502c487b59878ab5d3f940fcb8c889a1
Theis Mathiassen <theis.mathiassen@gmail.com>
2025-12-11 12:20:24 +0100

    fix: added exepected call to get shardowner now required
    Signed-off-by: Theis Mathiassen <theis.mathiassen@gmail.com>

--- service/sharddistributor/leader/process/processor_test.go
+++ service/sharddistributor/leader/process/processor_test.go
@@ -1568,6 +1568,16 @@ func TestLoadBalance_Convergence(t *testing.T) {
 		GlobalRevision:   10,
 	}, nil)
 
+	for sID := range shardStats {
+		var owner string
+		if _, ok := assignments[execA].AssignedShards[sID]; ok {
+			owner = execA
+		} else {
+			owner = execB
+		}
+		mocks.store.EXPECT().GetShardOwner(gomock.Any(), mocks.cfg.Name, sID).Return(&store.ShardOwner{ExecutorID: owner}, nil).AnyTimes()
+	}
+
 	mocks.election.EXPECT().Guard().Return(store.NopGuard())
 
 	mocks.store.EXPECT().AssignShards(gomock.Any(), mocks.cfg.Name, gomock.Any(), gomock.Any()).DoAndReturn(
@@ -1672,6 +1682,20 @@ func TestLoadBalance_BudgetConstraint(t *testing.T) {
 		ShardStats:       shardStats,
 		GlobalRevision:   10,
 	}, nil)
+
+	for sID := range shardStats {
+		var owner string
+		if _, ok := assignments[execA].AssignedShards[sID]; ok {
+			owner = execA
+		} else if _, ok := assignments[execB].AssignedShards[sID]; ok {
+			owner = execB
+		} else if _, ok := assignments[execC].AssignedShards[sID]; ok {
+			owner = execC
+		} else {
+			owner = execD
+		}
+		mocks.store.EXPECT().GetShardOwner(gomock.Any(), mocks.cfg.Name, sID).Return(&store.ShardOwner{ExecutorID: owner}, nil).AnyTimes()
+	}
 
 	mocks.election.EXPECT().Guard().Return(store.NopGuard())
 
@@ -1778,6 +1802,20 @@ func TestLoadBalance_ExecutorRemovedFromDestination(t *testing.T) {
 		ShardStats:       shardStats,
 		GlobalRevision:   10,
 	}, nil)
+
+	for sID := range shardStats {
+		var owner string
+		if _, ok := assignments[execA].AssignedShards[sID]; ok {
+			owner = execA
+		} else if _, ok := assignments[execB].AssignedShards[sID]; ok {
+			owner = execB
+		} else if _, ok := assignments[execC].AssignedShards[sID]; ok {
+			owner = execC
+		} else if _, ok := assignments[execF].AssignedShards[sID]; ok {
+			owner = execF
+		}
+		mocks.store.EXPECT().GetShardOwner(gomock.Any(), mocks.cfg.Name, sID).Return(&store.ShardOwner{ExecutorID: owner}, nil).AnyTimes()
+	}
 
 	mocks.election.EXPECT().Guard().Return(store.NopGuard())
 
