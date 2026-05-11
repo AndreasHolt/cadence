8ebcb8ce8e138d207e51221d421fea5dbe1128f2
Andreas Holt <6665487+AndreasHolt@users.noreply.github.com>
2026-05-11 14:56:47 +0200

    fix: cpu utilization sampling and ramping scalar
    Signed-off-by: Andreas Holt <6665487+AndreasHolt@users.noreply.github.com>

--- environment/kind-lab/scripts/sample-utilization.sh
+++ environment/kind-lab/scripts/sample-utilization.sh
@@ -30,6 +30,8 @@ matching_lab_pod() {
 
 read_pod_cgroup() {
   local pod="$1"
+  local sampled_at_usec
+  sampled_at_usec="$(date +%s%6N)"
   kubectl exec -n "$NAMESPACE" "$pod" -- sh -c '
     usage=$(awk "/usage_usec/ {print \$2}" /sys/fs/cgroup/cpu.stat)
     throttled=$(awk "/throttled_usec/ {print \$2}" /sys/fs/cgroup/cpu.stat)
@@ -37,7 +39,7 @@ read_pod_cgroup() {
     mem=$(cat /sys/fs/cgroup/memory.current)
     max=$(cat /sys/fs/cgroup/memory.max)
     printf "%s,%s,%s,%s,%s" "$usage" "$throttled" "$nr_throttled" "$mem" "$max"
-  '
+  ' | awk -v sampled_at_usec="$sampled_at_usec" '{print sampled_at_usec "," $0}'
 }
 
 collect_snapshot() {
@@ -72,23 +74,25 @@ write_delta_rows() {
 
   awk -F, -v ts="$timestamp" -v interval="$interval" '
     NR == FNR {
-      usage[$1] = $2
-      throttled[$1] = $3
-      nr_throttled[$1] = $4
+      sampled_at_usec[$1] = $2
+      usage[$1] = $3
+      throttled[$1] = $4
+      nr_throttled[$1] = $5
       next
     }
     ($1 in usage) {
-      usage_delta = $2 - usage[$1]
-      throttled_delta = $3 - throttled[$1]
-      throttled_events_delta = $4 - nr_throttled[$1]
-      if (usage_delta < 0 || throttled_delta < 0 || throttled_events_delta < 0) {
+      elapsed_seconds = ($2 - sampled_at_usec[$1]) / 1000000
+      usage_delta = $3 - usage[$1]
+      throttled_delta = $4 - throttled[$1]
+      throttled_events_delta = $5 - nr_throttled[$1]
+      if (elapsed_seconds <= 0 || usage_delta < 0 || throttled_delta < 0 || throttled_events_delta < 0) {
         next
       }
-      cpu_cores = usage_delta / (interval * 1000000)
-      throttled_cores = throttled_delta / (interval * 1000000)
+      cpu_cores = usage_delta / (elapsed_seconds * 1000000)
+      throttled_cores = throttled_delta / (elapsed_seconds * 1000000)
       throttled_events = throttled_events_delta
-      memory_mib = $5 / 1048576
-      printf "%s,%s,%.4f,%.4f,%d,%.2f,%s\n", ts, $1, cpu_cores, throttled_cores, throttled_events, memory_mib, $6
+      memory_mib = $6 / 1048576
+      printf "%s,%s,%.4f,%.4f,%d,%.2f,%s\n", ts, $1, cpu_cores, throttled_cores, throttled_events, memory_mib, $7
     }
   ' "$start_file" "$end_file" >> "$OUTPUT"
 }
